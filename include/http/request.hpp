264cb3ff00f31b07d3d65f6fe83e37b22323e68f
kewuaa <202321011111@std.uestc.edu.cn>
2025-02-25 19:59:10 +0800

    split request.cpp to several cpp file

--- include/http/request.hpp
+++ include/http/request.hpp
@@ -1,48 +1,65 @@
 #pragma once
 #include <string>
-#include <queue>
 #include <unordered_map>
 
+#include <asyncio.hpp>
 #include <magic_enum/magic_enum.hpp>
 
-
 #include "types.hpp"
+#include "buffer.hpp"
+using namespace kwa;
 
 
-namespace http {
-    class Request {
+namespace http::request {
+    struct Request {
+        bool valid { true };
+        Method method { 0 };
+        std::string path {};
+        std::unordered_map<std::string, std::string> params {};
+        std::string version {};
+        std::unordered_map<std::string, std::string> headers {};
+        std::string body {};
+        size_t body_size { 0 };
+        Request() = default;
+        Request(Request&) = delete;
+        Request& operator=(Request&) = delete;
+        Request(Request&&) noexcept;
+        Request& operator=(Request&&) noexcept;
+        const std::optional<size_t>& content_length() noexcept;
+        const std::optional<std::string>& content_type() noexcept;
+
+        inline auto method_str() const noexcept {
+            return magic_enum::enum_name(method);
+        }
+        private:
+            std::optional<size_t> _content_length { std::nullopt };
+            std::optional<std::string> _content_type { std::nullopt };
+    };
+
+    class Handler {
         public:
-            struct Data {
-                bool valid { true };
-                Method method { 0 };
-                std::string path {};
-                std::unordered_map<std::string, std::string> params {};
-                std::string version {};
-                std::unordered_map<std::string, std::string> headers {};
-                std::string body {};
-                Data() = default;
-                Data(Data&) = delete;
-                Data& operator=(Data&) = delete;
-                Data(Data&&) noexcept;
-                Data& operator=(Data&&) noexcept;
-                std::optional<size_t> content_length() const noexcept;
+            Handler() = delete;
+            Handler(Handler&) = delete;
+            Handler(Handler&&) = delete;
+            Handler operator=(Handler&) = delete;
+            Handler operator=(Handler&&) = delete;
+            Handler(asyncio::Socket&) noexcept;
+            asyncio::Task<> handle_request(request::Request&, std::string_view) noexcept;
+        private:
+            asyncio::Socket& _sock;
+            Buffer _buffer {};
+            asyncio::Task<> _handle_get(const request::Request&) noexcept;
+            asyncio::Task<> _handle_post(request::Request&, std::string_view) noexcept;
+    };
 
-                inline auto method_str() const noexcept {
-                    return magic_enum::enum_name(method);
-                }
-            };
-
-            Request() noexcept = default;
-            Request(Request&) = delete;
-            Request(Request&&) = delete;
-            Request& operator=(Request&) = delete;
-            Request& operator=(Request&&) = delete;
-            void parse(const char* buffer, size_t size) noexcept;
-            std::optional<Data> get() noexcept;
-
-            inline bool has_data() const noexcept {
-                return !_cache.empty();
-            }
+    class Parser {
+        public:
+            Parser() noexcept = default;
+            Parser(Parser&) = delete;
+            Parser(Parser&&) = delete;
+            Parser& operator=(Parser&) = delete;
+            Parser& operator=(Parser&&) = delete;
+            asyncio::Task<> process(const char* recv_buf, size_t size, Handler& handler) noexcept;
 
             inline void reset() noexcept {
                 _state = State::REQUEST_LINE;
@@ -56,17 +73,13 @@ namespace http {
                 BODY,
             };
 
-            Data _data {};
-            std::queue<Data> _cache {};
+            Buffer _buffer {};
+            Request _data {};
             State _state { State::REQUEST_LINE };
             std::string _incomplete_line {};
             void _move_next_state() noexcept;
             void _parse_request_line(std::string_view line) noexcept;
             void _parse_header(std::string_view line) noexcept;
             void _parse_line(std::string_view line) noexcept;
-
-            inline void _stage_data() noexcept {
-                _cache.push(std::exchange(_data, {}));
-            }
     };
 }
--- src/http/request.cpp
+++ src/http/request.cpp
@@ -1,23 +1,25 @@
 #include <spdlog/spdlog.h>
 
-#include "utils.hpp"
 #include "http/request.hpp"
 
 
-namespace http {
-    Request::Data::Data(Data&& data) noexcept:
+namespace http::request {
+    Request::Request(Request&& data) noexcept:
         valid(data.valid),
         method(data.method),
         path(std::move(data.path)),
         params(std::move(data.params)),
         version(std::move(data.version)),
         headers(std::move(data.headers)),
-        body(std::move(data.body))
+        body(std::move(data.body)),
+        body_size(std::exchange(data.body_size, 0)),
+        _content_length(std::exchange(data._content_length, std::nullopt)),
+        _content_type(std::exchange(data._content_type, std::nullopt))
     {
         //
     }
 
-    Request::Data& Request::Data::operator=(Data&& data) noexcept {
+    Request& Request::operator=(Request&& data) noexcept {
         valid = data.valid;
         method = data.method;
         path = std::move(data.path);
@@ -25,181 +27,45 @@ namespace http {
         version = std::move(data.version);
         headers = std::move(data.headers);
         body = std::move(data.body);
+        body_size = std::exchange(data.body_size, 0);
+        _content_length = std::exchange(data._content_length, std::nullopt);
+        _content_type = std::exchange(data._content_type, std::nullopt);
         return *this;
     }
 
-    std::optional<size_t> Request::Data::content_length() const noexcept {
+    const std::optional<size_t>& Request::content_length() noexcept {
+        if (_content_length.has_value()) {
+            return _content_length;
+        }
+
         constexpr const char* fields[] = {
             "Content-Length",
             "content-length",
         };
         for (size_t i = 0; i < sizeof(fields)/sizeof(const char*); i++) {
-            if (headers.find(fields[i]) != headers.end()) {
-                return std::make_optional(std::stoll(headers.at(fields[i])));
-            }
-        }
-        return std::nullopt;
-    }
-
-    void Request::_move_next_state() noexcept {
-        switch (_state) {
-            case State::REQUEST_LINE: {
-                _state = State::HEADERS;
-                break;
-            }
-            case State::HEADERS: {
-                if (_data.method != Method::POST) {
-                    _state = State::REQUEST_LINE;
-                } else {
-                    _state = State::BODY;
-                }
-                break;
-            }
-            case State::BODY: {
-                _state = State::REQUEST_LINE;
-                break;
-            }
-            default: {
-                std::unreachable();
+            if (headers.contains(fields[i])) {
+                _content_length = std::make_optional(std::stoll(headers.at(fields[i])));
                 break;
             }
         }
-        if (_state == State::REQUEST_LINE) {
-            _stage_data();
-        }
+        return _content_length;
     }
 
-    void Request::_parse_request_line(std::string_view line) noexcept {
-        auto space1 = line.find(' ', 0);
-        if (space1 == std::string_view::npos) {
-            _data.valid = false;
-            return;
+    const std::optional<std::string>& Request::content_type() noexcept {
+        if (_content_type.has_value()) {
+            return _content_type;
         }
-        auto method = magic_enum::enum_cast<Method>(line.substr(0, space1));;
-        if (!method) {
-            _data.valid = false;
-            return;
-        }
-        _data.method = *method;
-        auto space2 = line.find(' ', space1 + 1);
-        if (space2 == std::string_view::npos) {
-            _data.valid = false;
-            return;
-        }
-        auto path = line.substr(space1+1, space2-space1-1);
-        if (auto pos = path.find('?'); pos != std::string_view::npos) {
-            auto params = path.substr(pos+1);
-            path = path.substr(0, pos);
-            pos = 0;
-            while (pos < params.size()) {
-                auto split = params.find('&', pos);
-                if (split == std::string_view::npos) {
-                    split = params.size();
-                }
-                auto pair = params.substr(pos, split-pos);
-                auto equal_pos = pair.find('=');
-                _data.params[std::string(pair.substr(0, equal_pos))] = url_decode(pair.substr(equal_pos+1));
-                pos = split + 1;
+
+        constexpr const char* fields[] = {
+            "Content-Type",
+            "content-type",
+        };
+        for (size_t i = 0; i < sizeof(fields)/sizeof(const char*); i++) {
+            if (headers.contains(fields[i])) {
+                _content_type = std::make_optional(std::string_view(headers.at(fields[i])));
+                break;
             }
         }
-        _data.path = url_decode(path);
-        _data.version = line.substr(space2+1);
-        _move_next_state();
-    }
-
-    void Request::_parse_header(std::string_view line) noexcept {
-        auto split = line.find(':');
-        if (split == std::string_view::npos) {
-            _data.valid = false;
-            return;
-        }
-        auto key = line.substr(0, split);
-        auto value = line.substr(split+1);
-        while (value.starts_with(' ')) {
-            value = value.substr(1);
-        }
-        _data.headers[std::string(key)] = value;
-    }
-
-    void Request::_parse_line(std::string_view line) noexcept {
-        if (_state == State::REQUEST_LINE) {
-            _parse_request_line(line);
-        } else if (_state == State::HEADERS) {
-            _parse_header(line);
-        } else {
-            std::unreachable();
-        }
-    }
-
-    void Request::parse(const char* buffer, size_t size) noexcept {
-        if (size < 1) {
-            SPDLOG_WARN("try parse empty buffer");
-            return;
-        }
-        std::string_view view { buffer, size };
-        size_t pos = 0;
-        if (
-            _state != State::BODY
-            && !_incomplete_line.empty()
-            && _incomplete_line.back() == '\r'
-            && view[0] == '\n'
-        ) {
-            std::string_view line = _incomplete_line;
-            _parse_line(line.substr(0, line.size()-1));
-            _incomplete_line.clear();
-            pos++;
-        }
-        while (pos < size && _data.valid) {
-            if (_state == State::BODY) {
-                if (auto content_length = _data.content_length(); content_length) {
-                    if (auto remain_size = size - pos; remain_size < content_length) {
-                        _data.body += view.substr(pos);
-                        pos += remain_size;
-                    } else {
-                        _data.body += view.substr(pos, *content_length);
-                        _move_next_state();
-                        pos += *content_length;
-                    }
-                }
-            } else {
-                auto crlf = view.find(CRLF, pos);
-                if (crlf == std::string_view::npos) {
-                    _incomplete_line = view.substr(pos);
-                    break;
-                }
-                if (crlf == pos) {
-                    if (_incomplete_line.empty()) {
-                        _move_next_state();
-                    } else {
-                        _parse_line(_incomplete_line);
-                        _incomplete_line.clear();
-                    }
-                    pos += 2;
-                    continue;
-                }
-                if (!_incomplete_line.empty()) {
-                    std::string_view line { buffer + pos, buffer + crlf };
-                    _incomplete_line += line;
-                    _parse_line(_incomplete_line);
-                    _incomplete_line.clear();
-                } else {
-                    _parse_line({ buffer + pos, buffer + crlf });
-                }
-                pos = crlf + 2;
-            }
-        }
-        if (!_data.valid) {
-            _stage_data();
-            reset();
-        }
-    }
-
-    std::optional<Request::Data> Request::get() noexcept {
-        if (_cache.empty()) {
-            return std::nullopt;
-        }
-        auto res = std::move(_cache.front());
-        _cache.pop();
-        return std::make_optional(std::move(res));
+        return _content_type;
     }
 }
