60be673c3bad2f896a24addd308bd98280217462
kewuaa <202321011111@std.uestc.edu.cn>
2025-05-01 21:31:20 +0800

    add cli options

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,34 @@
+#include <getopt.h>
+
 #include "webserver.hpp"
 
 
-int main() noexcept {
+int main(int argc, char** argv) noexcept {
+    int c;
+    int max_listen_num = 1024;
+    short port = 12345;
+    while ((c = getopt(argc, argv, ":l:p:h")) != -1) {
+        switch (c) {
+            case 'l': {
+                max_listen_num = std::stoi(optarg);
+                break;
+            }
+            case 'p': {
+                port = std::stoi(optarg);
+                break;
+            }
+            case 'h':
+            case '?': {
+                std::fprintf(stderr, "%s [-p <port>] [-l <max listen num>]\n", argv[0]);
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
 #ifdef _DEBUG
     spdlog::set_level(spdlog::level::debug);
 #endif
     auto server = WebServer();
-    auto res = server.init("0.0.0.0", 12345, 10);
+    auto res = server.init("0.0.0.0", port, max_listen_num);
     if (!res) {
         SPDLOG_ERROR(res.error().message());
         exit(EXIT_FAILURE);
